8d0b768bef117872f1e2eccbdacb64bf5ab90f1a
Daniel Lappe <daniellappe@icloud.com>
2024-08-26 20:54:24 +0200

    Finished final lesson

--- main.py
+++ main.py
@@ -34,10 +34,17 @@ def main():
         for obj in updatable:
             obj.update(dt)
         
+        
+        
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
