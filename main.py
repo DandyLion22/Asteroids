b96c26f7ddbab045bd5e80f353cab66286a431fd
Daniel Lappe <daniellappe@icloud.com>
2025-09-08 17:30:51 +0200

    initialized black screen, basically created start of the game

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import pygame
-
+from constants import *
 
 def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
     print("Starting Asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+
 
 
 if __name__ == "__main__":
