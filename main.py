0bcd53ad4dcbe7d953a01430f87a254dd8858ff0
Daniel Lappe <daniellappe@icloud.com>
2025-09-08 17:41:06 +0200

    created clock object

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    print("Starting Asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
 
@@ -15,12 +21,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-
-
-    print("Starting Asteroids!")
-    print("Screen width: " + str(SCREEN_WIDTH))
-    print("Screen height: " + str(SCREEN_HEIGHT))
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
